1d845cae980ea43f028fd8fca5b596d5ef8c23c8
Declan Smith <dmansmith03@yahoo.com>
2021-02-25 10:19:03 -0600

    Initial Commit

---
+++ bill/billApp.swift
@@ -0,0 +1,17 @@
+//
+//  billApp.swift
+//  bill
+//
+//  Created by Declan Smith on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
